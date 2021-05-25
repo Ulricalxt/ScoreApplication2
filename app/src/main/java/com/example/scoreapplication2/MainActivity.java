df4e9fee33437478613162962d238da8a0643f25
李鑫熠 <2466758288@qq.com>
2021-05-25 20:19:42 +0800

    实现了+1，+2，+3的按钮的功能

--- app/src/main/java/com/example/scoreapplication2/MainActivity.java
+++ app/src/main/java/com/example/scoreapplication2/MainActivity.java
@@ -4,11 +4,91 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.scoreapplication2.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = "MainActivity";
+    private ActivityMainBinding mBinding;
+    private int scoreA,scoreB,saveA,saveB;
+    private final String KEY ="key";
+    private final String SP_A="sp_a";
+    private final String SP_B="sp_b";
+    private final String MY_SP="my_sp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        if (savedInstanceState!=null){
+            int[] i = savedInstanceState.getIntArray(KEY);
+            scoreA= i[0];
+            scoreB= i[1];
+            saveA = i[2];
+            saveB = i[3];
+
+        }
+        else {}
+        show();
+
+        mBinding.buttonAAdd1.setOnClickListener(v ->{
+            addA(1);
+        });
+        mBinding.buttonAAdd2.setOnClickListener(v ->{
+            addA(2);
+        });
+        mBinding.buttonAAdd3.setOnClickListener(v ->{
+            addA(3);
+        });
+        mBinding.buttonBAdd1.setOnClickListener(v ->{
+            addB(1);
+        });
+        mBinding.buttonBAdd2.setOnClickListener(v ->{
+            addB(2);
+        });
+        mBinding.buttonBAdd3.setOnClickListener(v ->{
+            addB(3);
+        });
+
+
+
+    }
+    /**
+     * 给A队加分 i要加的分数
+     * @param i 要加的分数
+     * */
+    public void addA(int i){
+        save();
+        scoreA += i;
+        mBinding.textScoreA.setText(String.valueOf(i));
+        show();
+
+    }
+    /**
+     * 给B队加分 i要加的分数
+     * @param i 要加的分数
+     * */
+    public void addB(int i){
+        save();
+        scoreB += i;
+        mBinding.textScoreB.setText(String.valueOf(i));
+        show();
+
+    }
+    /**
+     *保存数据
+     * */
+    public void save(){
+        saveA= Integer.parseInt(mBinding.textScoreA.getText().toString());
+        saveA= Integer.parseInt(mBinding.textScoreA.getText().toString());
+
+    }
+    /**
+     *显示数据
+     * */
+    public void show(){
+        mBinding.textScoreA.setText(String.valueOf(scoreA));
+        mBinding.textScoreB.setText(String.valueOf(scoreB));
     }
 }
